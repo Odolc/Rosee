36ec0bc0639623e52ac71532406b49795769b192
JEALG <jalain.gosse@icloud.com>
2019-11-10 16:05:38 +0100

    Prépararation Merge V4 + Correction Bug

--- desktop/php/rosee.php
+++ desktop/php/rosee.php
@@ -51,7 +51,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						<form class="form-horizontal">
 							<fieldset>
 								<div class="form-group">
-									<label class="col-sm-3 control-label">{{Nom de l'équipement caméra}}</label>
+									<label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
 									<div class="col-sm-6">
 										<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
 										<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement caméra}}"/>
@@ -170,8 +170,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
          </div>
     </div>
 </div>
-
-
 
 <?php include_file('desktop', 'rosee', 'js', 'rosee'); ?>
 <?php include_file('core', 'plugin.template', 'js'); ?>
