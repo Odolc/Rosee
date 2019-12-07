edf37df968eec8d261386391fad19c7f6e17495f
JEALG <jalain.gosse@icloud.com>
2019-12-07 16:56:15 +0100

    Correction Bugs
    - Ajout d’un cron 30 (Merci à kiboost)
    - Amélioration de l'affichage pour le Core V4 (Merci à kiboost)
    - Possibilité de renommer les commandes (Merci à kiboost)
    - Correction des historiques (Merci à kiboost)
    - Commande Refresh (sur la tuile, scénario etc) (Merci à kiboost)
    - Amélioration des logs
    - Correction type de generic
    - Correction Bug : l'actualisation des données ne se fait plus si l'équipement est désactivé
    - Les alertes sont visible par défaut (plus de masquage si l'alerte est à 0)
    - Nettoyage des dossiers (Merci à kiboost)
    - Mise à jour documentations
    
    Co-Authored-By: KiboOst <kiboost@users.noreply.github.com>

---
+++ plugin_info/configuration.php
@@ -0,0 +1,38 @@
+<?php
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+include_file('core', 'authentification', 'php');
+if (!isConnect()) {
+	include_file('desktop', '404', 'php');
+	die();
+}
+?>
+
+<script>
+    $(function() {
+        $('#div_confPlugin > .panel.panel-primary').hide()
+    })
+
+    $("input[data-l1key='functionality::cron5::enable']").on('change',function(){
+        if ($(this).is(':checked')) $("input[data-l1key='functionality::cron30::enable']").prop("checked", false)
+    });
+
+    $("input[data-l1key='functionality::cron30::enable']").on('change',function(){
+        if ($(this).is(':checked')) $("input[data-l1key='functionality::cron5::enable']").prop("checked", false)
+    });
+</script>
